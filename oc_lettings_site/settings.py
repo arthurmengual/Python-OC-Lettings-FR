92462483c282a5fc69a4690225dfee309da21c90
arthur <arthur.mengual@gmail.com>
2022-04-22 19:31:36 -0300

    commit

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='Foo')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'oc-lettings-site29.herokuapp.com/']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'oc-lettings-site29.herokuapp.com']
 
 
 # Application definition
